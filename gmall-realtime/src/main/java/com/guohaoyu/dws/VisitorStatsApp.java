53dc4d0213b1c6294c5b7a32ade5b214d739b199
ghy <2778500477@qq.com>
2022-01-12 10:51:39 +0800

    dws

--- gmall-realtime/src/main/java/com/guohaoyu/dws/VisitorStatsApp.java
+++ gmall-realtime/src/main/java/com/guohaoyu/dws/VisitorStatsApp.java
@@ -1,22 +1,41 @@
 package com.guohaoyu.dws;
 
+import com.guohaoyu.util.MyKafkaUtil;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+
 public class VisitorStatsApp {
     public static void main(String[] args) {
         //获取执行环境
+        //TODO 0.基本环境准备
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        //设置并行度
+        env.setParallelism(1);
+        /*
+        //检查点CK相关设置
+        env.enableCheckpointing(5000, CheckpointingMode.AT_LEAST_ONCE);
+        env.getCheckpointConfig().setCheckpointTimeout(60000);
+        StateBackend fsStateBackend = new FsStateBackend(
+                "hdfs://hadoop102:8020/gmall/flink/checkpoint/VisitorStatsApp");
+        env.setStateBackend(fsStateBackend);
+        System.setProperty("HADOOP_USER_NAME","atguigu");
+        */
 
-        //设置ck
+        //TODO 1.从Kafka的pv、uv、跳转明细主题中获取数据
+        String groupId = "visitor_stats_app";
+        String pageViewSourceTopic = "dwd_page_log";
+        String uniqueVisitSourceTopic = "dwm_unique_visit";
+        String userJumpDetailSourceTopic = "dwm_user_jump_detail";
 
-        //设置ck的一致性语义
+        FlinkKafkaConsumer<String> pageViewSource = MyKafkaUtil.getKafkaSource(pageViewSourceTopic, groupId);
+        FlinkKafkaConsumer<String> uniqueVisitSource = MyKafkaUtil.getKafkaSource(uniqueVisitSourceTopic, groupId);
+        FlinkKafkaConsumer<String> userJumpSource = MyKafkaUtil.getKafkaSource(userJumpDetailSourceTopic, groupId);
 
-        //设置任务完成之后保存最后一次的ck
+        DataStreamSource<String> pageViewDStream = env.addSource(pageViewSource);
+        DataStreamSource<String> uniqueVisitDStream = env.addSource(uniqueVisitSource);
+        DataStreamSource<String> userJumpDStream = env.addSource(userJumpSource);
 
-        //设置ck的自动重启策略
-
-        //设置状态后端
-
-        //设置用户名
-
-        //读取kafka数据
 
         //统一数据格式
 
