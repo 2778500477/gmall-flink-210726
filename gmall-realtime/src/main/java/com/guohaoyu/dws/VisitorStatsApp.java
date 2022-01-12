948de03ce195f5bb8ebe919a87b8f06d681d726a
ghy <2778500477@qq.com>
2022-01-12 10:48:13 +0800

    dws

---
+++ gmall-realtime/src/main/java/com/guohaoyu/dws/VisitorStatsApp.java
@@ -0,0 +1,34 @@
+package com.guohaoyu.dws;
+
+public class VisitorStatsApp {
+    public static void main(String[] args) {
+        //获取执行环境
+
+        //设置ck
+
+        //设置ck的一致性语义
+
+        //设置任务完成之后保存最后一次的ck
+
+        //设置ck的自动重启策略
+
+        //设置状态后端
+
+        //设置用户名
+
+        //读取kafka数据
+
+        //统一数据格式
+
+        //union多个流
+
+        //提取时间生成WaterMark
+
+        //分组开窗聚合
+
+        //将数据写入clickHouse
+
+        //启动任务
+
+    }
+}
