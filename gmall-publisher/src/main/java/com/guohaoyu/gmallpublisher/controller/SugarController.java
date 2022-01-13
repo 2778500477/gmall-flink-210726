d0a125f0976db1ecc664e91d09b5ddfa8298e70c
ghy <2778500477@qq.com>
2022-01-13 11:24:54 +0800

    ads

--- gmall-publisher/src/main/java/com/guohaoyu/gmallpublisher/controller/SugarController.java
+++ gmall-publisher/src/main/java/com/guohaoyu/gmallpublisher/controller/SugarController.java
@@ -8,19 +8,31 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
 
 //@Controller
 @RestController
+@RequestMapping("/api/sugar")
 public class SugarController {
     @Autowired
     ProductStatsService productStatsService;
-    @RequestMapping("/api/sugar/gmv")
-    public String getGmv(@RequestParam("date") int date){
+    @RequestMapping("/gmv")
+    public String getGmv(@RequestParam(value = "date",defaultValue = "0") int date){
+        if (date==0){
+          date=getToday();
+        }
+
         BigDecimal gmv = productStatsService.getGmv(date);
         return "{\n" +
                 "    \"status\": 0,\n" +
                 "    \"msg\": \"\",\n" +
                 "    \"data\": "+gmv+"\n" +
                 "}\n";
+    }
+
+    private int getToday() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+        long ts = System.currentTimeMillis();
+        return Integer.parseInt(sdf.format(ts));
     }
 }
